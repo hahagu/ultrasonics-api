d4b9468ea60c53f9e7b0ee6d57d0e75cbbd306a9
Min Gu Kang <mingu2001@gmail.com>
2023-02-12 17:21:48 +0800

    📌 Temporarily disable redirect uri setting 📌

--- ultrasonics_api/services/spotify.py
+++ ultrasonics_api/services/spotify.py
@@ -89,7 +89,7 @@ def api_spotify_auth_request():
     params = {
         "client_id": os.environ.get('SPOTIFY_CLIENT_ID'),
         "response_type": "code",
-        "redirect_uri": os.environ.get('SPOTIFY_REDIRECT_URI') + "api/spotify/auth",
+        "redirect_uri": "http://localhost:8003/api/spotify/auth",
         "state": str(uuid4()),
         "scope": " ".join([
             "playlist-modify-public",
